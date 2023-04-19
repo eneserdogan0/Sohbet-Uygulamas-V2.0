fabf9e6f872e0cdfac434ba2642680a9b681144f
eneserdogan0 <eneserdogan4242@gmail.com>
2023-04-20 00:48:07 +0300

    Yeni giriş düzenlemesi

--- Sohbet Uygulaması V2.0/Login.cs
+++ Sohbet Uygulaması V2.0/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sohbet_Uygulaması_V2._0
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
